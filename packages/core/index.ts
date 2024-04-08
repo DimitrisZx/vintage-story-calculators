f294d2affd38f6039e08efa7e499f888fa6c81b3
DimitrisZar <dimitriszarahanis@gmail.com>
2024-04-08 22:50:20 +0300

    silly change

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,27 +1,28 @@
-export function getApproximateCharcoal(fireWoodStacks: number, optimistic?: boolean) {
-    if (fireWoodStacks < 1) return 0;
+export function getApproximateCharcoal(
+  fireWoodStacks: number,
+  optimistic?: boolean
+) {
+  if (fireWoodStacks < 1) return 0;
 
-    const factor = (() => {
-        if (optimistic === true){
-            return 7
-        } else if (optimistic === false) {
-            return 4;
-        }
-        return 5.5;
-        }
-    )()
+  const factor = (() => {
+    if (optimistic === true) {
+      return 7;
+    } else if (optimistic === false) {
+      return 4;
+    }
+    return 5.5;
+  })();
 
-    return fireWoodStacks * factor;
+  return fireWoodStacks * factor;
 }
 export function getFireWoodStacks(logs: number) {
-    
-    return logs/4;
+  return logs / 4;
 }
 
 function main() {
-    const logs = 4;
-    const fireWoodStacks = getFireWoodStacks(logs);
-    console.log(getApproximateCharcoal(fireWoodStacks, false));
+  const logs = 4;
+  const fireWoodStacks = getFireWoodStacks(logs);
+  console.log(getApproximateCharcoal(fireWoodStacks, false));
 }
 
-main()
+main();
