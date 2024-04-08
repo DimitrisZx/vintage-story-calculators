4549fe96413dd4e6003b459ae7fedce65dcf02f5
DimitrisZar <dimitriszarahanis@gmail.com>
2024-04-09 01:16:03 +0300

    add test

--- packages/core/index.test.ts
+++ packages/core/index.test.ts
@@ -1,22 +1,28 @@
-import { expect, test } from 'bun:test';
-import { getApproximateCharcoal, getFireWoodStacks } from '.';
+import { expect, test } from "bun:test";
+import { getApproximateCharcoal, getFireWoodStacks } from ".";
 
 const fireWoodStacksCases = [
-    [0,0],
-    [32,8]
-]
+  [0, 0],
+  [32, 8],
+];
 
-test.each(fireWoodStacksCases)
-("getFireWoodStacks", (a,b) => {
-    expect(getFireWoodStacks(a)).toBe(b);
-})
+const charcoalCases: [number, boolean | undefined, number][] = [
+  [32, false, 128],
+  [32, true, 224],
+  [32, undefined, 176],
+];
 
-const charcoalCases: [number, boolean|undefined, number][] = [
-    [32, false, 128],
-    [32, true, 224],
-    [32, undefined, 176],
-]
+test.each(fireWoodStacksCases)("getFireWoodStacks", (a, b) => {
+  expect(getFireWoodStacks(a)).toBe(b);
+});
 
 test.each(charcoalCases)("getApproximateCharcoal", (a, b, result) => {
-    expect(getApproximateCharcoal(a,b)).toBe(result);
-})
+  expect(getApproximateCharcoal(a, b)).toBe(result);
+});
+
+test("Integration", () => {
+  const logs = 4;
+  const fireWoodStacks = getFireWoodStacks(logs);
+  const charcoal = getApproximateCharcoal(fireWoodStacks, false);
+  expect(charcoal).toBe(4);
+});
